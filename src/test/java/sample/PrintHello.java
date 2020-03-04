c71cbc4ea43b827c5936a1e0b7f05bbccc40a712
NamrathaNarayanaswamy <61141434+NamrathaNarayanaswamy@users.noreply.github.com>
2020-03-04 15:12:07 +0100

    Add files via upload

---
+++ src/test/java/sample/PrintHello.java
@@ -0,0 +1,12 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class PrintHello {
+
+    @Test
+    public static void main() {
+        System.out.println("Hello Hello");
+        System.out.println("Executed");
+    }
+}
